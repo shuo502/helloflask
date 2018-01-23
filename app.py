e1a36874703a7c71f4a40209c68d35c0df7fbc46
flask <shuo502@163.com>
2018-01-23 16:16:31 +0800

    flask app

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
+@app.rout('/in/')
+def inx():
+    pass
+    return 'x'
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
