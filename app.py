4d9f4dd6a30df7ea9a536a3e3872dcea2b277715
flask <shuo502@163.com>
2018-01-23 16:11:11 +0800

    flask app

---
+++ app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: mum
+@license: Apache Licence 
+@contact: shuo502@163.com
+@author: ‘yo‘
+@site: http://github.com/shuo502
+@software: PyCharm
+@file: app.py.py
+@time: 2018/1/23 15:29
+"""
+from flask import *
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
