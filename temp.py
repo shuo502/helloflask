a27b163465c75a7e8c1b98c0912528652008abbc
flask <shuo502@163.com>
2018-01-23 16:14:52 +0800

    flask app

---
+++ temp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: mum
+@license: Apache Licence 
+@contact: shuo502@163.com
+@author: ‘yo‘
+@site: http://github.com/shuo502
+@software: PyCharm
+@file: temp.py
+@time: 2018/1/23 16:14
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
+
